3b085ec288fd4f2063d304f462895a280735e4db
Amash Ansari <42666741+iamamash@users.noreply.github.com>
2023-11-13 13:36:14 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello from Batch 3!",
+        message: "Hello From Batch 3!",
       },
       null,
       2
